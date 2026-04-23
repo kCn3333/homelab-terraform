6fd55b89937f7a98b4d2b87e424d4af5a25f7ff7
kCn3333 <pawelkwiecienkrk@gmail.com>
2026-04-23 10:29:33 +0200

    add: aws_iam

--- iam_user.tf
+++ iam_user.tf
@@ -4,10 +4,10 @@ variable "aws_account_name" {
 
 variable "names" {
   default = ["johny", "michael", "sarah", "emily"]
-} 
+}
 
 resource "aws_iam_user" "kCn_user" {
   count = length(var.names)
-  name= var.names[count.index]
+  name  = var.names[count.index]
   #name = "${var.aws_account_name}_${count.index + 1}"
 }
