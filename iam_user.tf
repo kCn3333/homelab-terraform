b336da6908e0369af93552309823cfb08e8e99ab
kCn3333 <pawelkwiecienkrk@gmail.com>
2026-04-23 08:20:31 +0200

    add: aws_iam

--- iam_user.tf
+++ iam_user.tf
@@ -1,3 +1,13 @@
+variable "aws_account_name" {
+  default = "kCn_user"
+}
+
+variable "names" {
+  default = ["johny", "michael", "sarah", "emily"]
+} 
+
 resource "aws_iam_user" "kCn_user" {
-  name = "kCn_user"
+  count = length(var.names)
+  name= var.names[count.index]
+  #name = "${var.aws_account_name}_${count.index + 1}"
 }
